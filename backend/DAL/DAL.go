c80225c4cd1fe465668ccfb9e5141c353d5aef94
Lililatortue <will.descoteaux@gmail.com>
2024-11-17 20:12:41 -0500

    initialisation du projet creation de package DAL et BLL, inclut une petite list TODO 	new file:   backend/BLL/BLL.go 	new file:   backend/DAL/DAL.go 	new file:   backend/go.mod

---
+++ backend/DAL/DAL.go
@@ -0,0 +1,11 @@
+package DAL
+
+//TODO: create package DAL (un dossier)
+//TODO: DAL go get driver for oracle sql
+//TODO: Import sql lib
+
+//TODO: Create DAL
+
+//TODO: Create Repo
+
+//TODO: Create Mapping
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/colXavGig/XavierGiguereWilliamDescoteauxAlexandreEmond_pf_8inf851
+
+go 1.23.3
