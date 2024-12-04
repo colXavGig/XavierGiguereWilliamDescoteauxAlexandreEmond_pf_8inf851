20a38c2b9a45d36c70357db6d3b79041f47737c7
Xavier G <xavier.giguere@lcieducation.net>
2024-12-04 13:05:26 -0500

    init DB

--- backend/DAL/DAL.go
+++ backend/DAL/DAL.go
@@ -1,5 +1,13 @@
 package DAL
 
+import (
+	"database/sql"
+)
+
+type OracleDB struct{
+	*sql.DB
+}
+
 //TODO: create package DAL (un dossier)
 //TODO: DAL go get driver for oracle sql
 //TODO: Import sql lib
